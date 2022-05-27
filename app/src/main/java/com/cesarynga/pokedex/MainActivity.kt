65b6b06f23bdbf72a67f16d7faf2e9ac37f6ed5c
César Ynga <csarynga@gmail.com>
2022-05-27 16:50:20 -0500

    migrate to material3

--- app/src/main/java/com/cesarynga/pokedex/MainActivity.kt
+++ app/src/main/java/com/cesarynga/pokedex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cesarynga.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,18 +15,38 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             val navController = rememberNavController()
 
             NavHost(navController = navController, startDestination = "pokemonList") {
                 composable("pokemonList") { PokemonList(navController = navController) }
                 composable("pokemonDetails") {
-                    val pokemon = navController.previousBackStackEntry?.savedStateHandle?.get<Pokemon>("pokemon")
+                    val pokemon =
+                        navController.previousBackStackEntry?.savedStateHandle?.get<Pokemon>("pokemon")
                     pokemon?.let {
                         PokemonDetails(navController = navController, it)
                     }
                 }
             }
+
+//            PokedexTheme {
+//                Box(
+//                    Modifier
+//                        .background(MaterialTheme.colors.primary)
+//                        .statusBarsPadding()
+//                ) {
+//                    Box(
+//                        Modifier
+//                            .background(Color.Red)
+//                            .displayCutoutPadding()
+//                    ) {
+//                        // app content
+//                    }
+//                }
+//            }
         }
     }
 }
