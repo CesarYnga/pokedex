e13870a37c55618fbeff88a72b2fc16639f359cd
César Ynga <csarynga@gmail.com>
2023-03-03 20:03:46 -0500

    get pokemon by id

---
+++ app/src/main/java/com/cesarynga/pokedex/data/source/PokemonTypeModel.kt
@@ -0,0 +1,3 @@
+package com.cesarynga.pokedex.data.source
+
+data class PokemonTypeModel()
---
+++ app/src/main/java/com/cesarynga/pokedex/data/source/remote/response/PokemonTypeResponse.kt
@@ -0,0 +1,3 @@
+package com.cesarynga.pokedex.data.source.remote.response
+
+data class PokemonTypeResponse()
