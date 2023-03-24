3663da2e9a23415e2ef00f916f6a149b1e8ae2eb
César Ynga <csarynga@gmail.com>
2023-03-23 22:44:42 -0500

    add pokemon type color and icon

--- app/src/main/java/com/cesarynga/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/cesarynga/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,7 +13,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,6 +25,7 @@ import coil.request.ImageRequest
 import com.cesarynga.pokedex.pokemons.domain.model.Pokemon
 import com.cesarynga.pokedex.pokemons.domain.model.PokemonType
 import com.cesarynga.pokedex.ui.theme.PokedexTheme
+import com.cesarynga.pokedex.ui.theme.PokemonTypeColor
 import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -67,12 +72,33 @@ fun PokemonDetailHeader(pokemon: Pokemon, modifier: Modifier = Modifier) {
 
 @Composable
 fun PokemonTypeView(pokemonType: PokemonType, modifier: Modifier = Modifier) {
-    Text(
-        text = pokemonType.name,
+    Row(
         modifier = modifier
-            .background(color = Color.Cyan, shape = RoundedCornerShape(4.dp))
-            .padding(horizontal = 16.dp, vertical = 4.dp)
-    )
+            .background(
+                color = PokemonTypeColor.getColorByTypeId(pokemonType.id),
+                shape = RoundedCornerShape(4.dp)
+            )
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(
+                id = LocalContext.current.resources.getIdentifier(
+                    "ic_pokemon_type_${pokemonType.name.lowercase()}",
+                    "drawable",
+                    LocalContext.current.packageName
+                )
+            ),
+            contentDescription = "",
+            tint = Color.White
+        )
+        Spacer(modifier = modifier.size(8.dp))
+        Text(
+            text = pokemonType.name,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+    }
 }
 
 @Composable
--- app/src/main/java/com/cesarynga/pokedex/ui/theme/Color.kt
+++ app/src/main/java/com/cesarynga/pokedex/ui/theme/Color.kt
@@ -6,3 +6,31 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// Pokemon type colors
+enum class PokemonTypeColor(val color: Color) {
+    Normal(Color(0xFFB3B5B2)),
+    Fighting(Color(0xFFEB4971)),
+    Flying(Color(0xFFB8A5F2)),
+    Poison(Color(0xFFc582d9)),
+    Ground(Color(0xFFF78551)),
+    Rock(Color(0xFFd4c9a1)),
+    Bug(Color(0xFFB5C534)),
+    Ghost(Color(0xFF7f8ac9)),
+    Steel(Color(0xFF78aab5)),
+    Fire(Color(0xFFF4934D)),
+    Water(Color(0xFF75b1e5)),
+    Grass(Color(0xFF7fca79)),
+    Electric(Color(0xFFF9DF78)),
+    Psychic(Color(0xFFfb9d9a)),
+    Ice(Color(0xFF91d9cd)),
+    Dragon(Color(0xFF8656FA)),
+    Dark(Color(0xFF7a7981)),
+    Fairy(Color(0xFFf1a6eb));
+
+    companion object {
+        fun getColorByTypeId(typeId: Int): Color {
+            return values()[typeId - 1].color
+        }
+    }
+}
